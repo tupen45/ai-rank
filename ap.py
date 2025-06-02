f1c53ee848f490c7de7b328049232a2bc849f72d
Tbhcr <43102977+tupen45@users.noreply.github.com>
2025-06-02 10:54:34 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -46,4 +46,5 @@ def predict():
         return jsonify({'error': 'Invalid category'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
