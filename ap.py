b7b8331f956571166aaba41d47dd151c12f16244
Tbhcr <43102977+tupen45@users.noreply.github.com>
2025-06-02 10:59:36 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from flask_cors import CORS
